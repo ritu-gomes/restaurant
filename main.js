6c225c8da5f1822b1cba4ca3e4e72a661b2868da
ritu-gomes <irengomes018@gmail.com>
2020-06-14 19:08:38 +0600

    responsive

---
+++ main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    var x = document.getElementById("res-menu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
